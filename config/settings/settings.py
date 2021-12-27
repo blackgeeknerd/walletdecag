7bf52661f2daa55c69fd9377ddc0a50911814836
Blackgeek <blackgeeknerd@gmail.com>
2021-12-27 03:15:43 +0100

    Documentation bug

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -49,8 +49,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wallet',
-    # 'drf_yasg',
-    'drf_yasg2',
     'rest_framework',
     'rest_framework.authtoken',
 ]
