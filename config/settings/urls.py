aea2fa9613e2a735bf3d8895c33f54de855a5256
Blackgeek <blackgeeknerd@gmail.com>
2021-12-27 03:03:32 +0100

    added the api documentation

--- config/settings/urls.py
+++ config/settings/urls.py
@@ -16,24 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg2.views import get_schema_view
-from drf_yasg2 import openapi
+# from drf_yasg2.views import get_schema_view
+# from drf_yasg2 import openapi
 
 
 
 #Added for the swagger/redoc documentation Api
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="Snippets API",
+#       default_version='v1',
+#       description="Test description",
+#       terms_of_service="https://www.google.com/policies/terms/",
+#       contact=openapi.Contact(email="contact@snippets.local"),
+#       license=openapi.License(name="BSD License"),
+#    ),
+#    public=True,
+#    permission_classes=(permissions.AllowAny,),
+# )
 
 urlpatterns = [
     path("", include('wallet.urls')),
@@ -42,6 +42,6 @@ urlpatterns = [
     
 #    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 #    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+#    path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
    
 ]
