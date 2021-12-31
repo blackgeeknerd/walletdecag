11138dc0cec9b5e8a66665abac9206d8a4d9ce9c
Blackgeek <blackgeeknerd@gmail.com>
2021-12-31 05:50:36 +0100

    refactor models

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -141,5 +141,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 
